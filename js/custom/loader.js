826f42195fad2d108fe95f8e7f46d3248e4b449c
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-21 21:47:44 +0530

    auto-update-loader.js(21:47:41)

--- js/custom/loader.js
+++ js/custom/loader.js
@@ -61,6 +61,6 @@ async function jsInit(){
 
 for(i=2;i<26;i++){
     for(j=1;j<3;j++){
-        document.querySelector(`#tbl1 > tbody > tr:nth-child(2) > td:nth-child(${j})`).innerHTML
+        document.querySelector(`#tbl1 > tbody > tr:nth-child(${i}) > td:nth-child(${j})`).innerHTML
     }
 }
