2ab2d082e27e29a6134e07664e837cad83efae90
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-21 22:07:27 +0530

    auto-update-loader.js(22:07:23)

--- js/custom/loader.js
+++ js/custom/loader.js
@@ -8,7 +8,6 @@ loading.background  = 'black'
 loading.font        = 'monospace'
 loading.fontSize    = '22pt'
 loading.charArray   =  ["Α","α","Β","β","Γ","γ","Δ","δ","Ε","ε","Ζ","ζ","Η","η","Θ","θ","Ι","ι","Κ","κ","Λ","λ","Μ","μ","Ν","ν","Ξ","ξ","Ο","ο","Π","π","Ρ","ρ","Σ","σ","ς","Τ","τ","Υ","υ","Φ","φ","Χ","χ","Ψ","ψ","Ω","ω"]
-loading.fontColor   = 'green'
 loading.maximize    = true
 loading.random      = true
 loading.randomColor = 1
