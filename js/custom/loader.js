452ae627100f7c74a4e1e535ec15e74e4fbf17ef
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-21 21:54:10 +0530

    auto-update-loader.js(21:54:06)

--- js/custom/loader.js
+++ js/custom/loader.js
@@ -3,7 +3,7 @@ let [error,sucess] = ['','']
 console.green = (a)=>console.log('%c'+a,'color:green')
 
 let loading = new Matrix('background')
-loading.timeout     = 150       
+loading.timeout     = 70      
 loading.background  = 'black'
 loading.font        = 'monospace'
 loading.fontSize    = '10pt'
