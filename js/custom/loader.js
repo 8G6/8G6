731a4d28c1ed4286fa19b7caa6169e31a9728f51
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-21 22:08:32 +0530

    auto-update-loader.js(22:08:29)

--- js/custom/loader.js
+++ js/custom/loader.js
@@ -6,7 +6,7 @@ let loading = new Matrix('background')
 
 loading.background  = 'black'
 loading.font        = 'monospace'
-loading.fontSize    = '22pt'
+loading.fontSize    = '2pt'
 loading.charArray   =  ["Α","α","Β","β","Γ","γ","Δ","δ","Ε","ε","Ζ","ζ","Η","η","Θ","θ","Ι","ι","Κ","κ","Λ","λ","Μ","μ","Ν","ν","Ξ","ξ","Ο","ο","Π","π","Ρ","ρ","Σ","σ","ς","Τ","τ","Υ","υ","Φ","φ","Χ","χ","Ψ","ψ","Ω","ω"]
 loading.maximize    = true
 loading.random      = true
