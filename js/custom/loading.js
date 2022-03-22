cd523ce4fdaa85b0eac97abc7b7081a3ef8f9f2c
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-22 21:30:29 +0530

    auto-update-loading.js(21:30:27)

--- js/custom/loading.js
+++ js/custom/loading.js
@@ -2,11 +2,12 @@
 j=0;
 
 let k = setInterval(()=>{
-    document.querySelector('#dev').style.width = `${j}%`
-    console.log(j)
     if(j<=80.1)
     j+=0.1
     else
     clearInterval(k)
+    document.querySelector('#dev').style.width = `${j}%`
+    console.log(j)
+   
 },1)
 
