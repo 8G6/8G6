e233519205b7bc406f2d9aaab7edea0b90dd7404
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-22 21:23:17 +0530

    auto-update-loading.js(21:23:15)

--- js/custom/loading.js
+++ js/custom/loading.js
@@ -1,24 +1,12 @@
-function predict(x,bool){
-    let coff1 = [7.812500000191304e-8, -0.000017708333333530185, 0.0014687500000070603, -0.05291666666691738, 1.2000000000034017, -3.5185366870743305e-12]
-    let coff2 = [0.00006597222222223214, -0.007708333333335091, 0.45992063492068813, 14.809523809523572]
-    let i;
-    let n = bool ? coff1 : coff2
-    let out = 0;
-    for(i=0;i<n.length;i++){
-        out+=n[i] * x ** (n.length-i-1)
-    }
-    return out
-}
 
 j=0;
 
 let k = setInterval(()=>{
-    document.querySelector('#dev').style.width = `${predict(j,1)}%`
-    document.querySelector('#dev').style.left  = `${predict(j,0)}%` 
-    console.log(j,predict(j,1),predict(j,0))
-    if(j<100)
-    j++
+    document.querySelector('#dev').style.width = `${j}%`
+    console.log(j)
+    if(j<79.1)
+    j+=0.1
     else
     clearInterval(k)
-},10)
+},1)
 
