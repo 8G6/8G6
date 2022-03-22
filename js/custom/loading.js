2ac81f3184cdcb182d89db83c2fb3484a8658e8d
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-22 19:30:53 +0530

    auto-update-loading.js(19:30:52)

--- js/custom/loading.js
+++ js/custom/loading.js
@@ -16,6 +16,7 @@ let k = setInterval(()=>{
     document.querySelector('#dev').style.width = `${predict(i,1)}%`
     document.querySelector('#dev').style.left  = `${predict(i,0)}%` 
     console.log(i,document.querySelector('#dev').style)
-    if(i==0)}
+    if(i==100)
+    i=
 },100)
 
