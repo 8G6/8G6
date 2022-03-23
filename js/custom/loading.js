178cc462a18900372c8a92c3f9193307e9d22d67
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 18:08:45 +0530

    auto-update-loading.js(18:08:42)

--- js/custom/loading.js
+++ js/custom/loading.js
@@ -21,7 +21,7 @@ let max_x = disp.scrollWidth
 let max_y = disp.scrollHeight
 let [bool_x,bool_y] = [1,1]
 
-function get(x,y){
+function get(x,y,x){
 
     if(x>=120 && x<=max_x-121 && bool_x) x++
     else{
