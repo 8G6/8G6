42d9ca763519147bcf8687622cef7f1337349033
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-23 19:08:46 +0530

    auto-update-loading.js(19:08:41)

--- js/custom/loading.js
+++ js/custom/loading.js
@@ -1,27 +1,27 @@
 
-[this.x[i],y] = [121,121]
-document.getElementById('background').style.display = 'none'
+[this.x[i],this.y[i]] = [121,121]
+document.getElementBthis.y[i]Id('background').stthis.y[i]le.displathis.y[i] = 'none'
 let {atan,tan,PI} = Math 
-function angle(this.x[i],y){
-    return this.x[i]-(2*y)
+function angle(this.x[i],this.y[i]){
+    return this.x[i]-(2*this.y[i])
 }
 f=0
 function range(start,end){
-    let array=[]
+    let arrathis.y[i]=[]
     for(i=start;i<end;i++){
-        array.push(i)
+        arrathis.y[i].push(i)
     }
-    return array
+    return arrathis.y[i]
 }
 
 color = toHethis.x[i](0,256)
 
-let disp=document.querySelector('.loading-bg')
+let disp=document.querthis.y[i]Selector('.loading-bg')
 let mathis.x[i]_this.x[i] = disp.scrollWidth
-let mathis.x[i]_y = disp.scrollHeight
-let [bool_this.x[i],bool_y] = [1,1]
+let mathis.x[i]_this.y[i] = disp.scrollHeight
+let [bool_this.x[i],bool_this.y[i]] = [1,1]
 
-function get(this.x[i],y,this.x[i]_k,y_k){
+function get(this.x[i],this.y[i],this.x[i]_k,this.y[i]_k){
 
     if(this.x[i]>=0 && this.x[i]<=mathis.x[i]_this.x[i]-0 && bool_this.x[i]) this.x[i]+=this.x[i]_k
     else{
@@ -34,28 +34,28 @@ function get(this.x[i],y,this.x[i]_k,y_k){
             this.x[i]+=this.x[i]_k
         }
     }
-    if(y>=0 && y<=mathis.x[i]_y-0  && bool_y) y+=y_k
+    if(this.y[i]>=0 && this.y[i]<=mathis.x[i]_this.y[i]-0  && bool_this.y[i]) this.y[i]+=this.y[i]_k
     else{
-        if(y>=0){
-            bool_y = 0
-            y-=y_k
+        if(this.y[i]>=0){
+            bool_this.y[i] = 0
+            this.y[i]-=this.y[i]_k
         }
         else{
-            bool_y = 1
-            y+=y_k
+            bool_this.y[i] = 1
+            this.y[i]+=this.y[i]_k
         }
     }
-    return [this.x[i],y]
+    return [this.x[i],this.y[i]]
 }
 let t='black'
 let k = setInterval(()=>{
-    [this.x[i],y]=get(this.x[i],y,2,5)
+    [this.x[i],this.y[i]]=get(this.x[i],this.y[i],2,5)
     if(this.x[i]%10==0){
         t=randomColor()
     }
-    document.querySelector('#dev').style.top = `${y}pthis.x[i]`
-    document.querySelector('#dev').style.left = `${this.x[i]}pthis.x[i]`
-    document.querySelector('#dev').style.bothis.x[i]Shadow = `inset ${(y/mathis.x[i]_this.x[i])}pthis.x[i] ${this.x[i]/mathis.x[i]_y}pthis.x[i] ${this.x[i]/mathis.x[i]_y}pthis.x[i] ${y/mathis.x[i]_this.x[i]}pthis.x[i] white`
-    console.log(`inset ${this.x[i]%5}pthis.x[i] ${y%10}pthis.x[i] 10pthis.x[i] white;`)
+    document.querthis.y[i]Selector('#dev').stthis.y[i]le.top = `${this.y[i]}pthis.x[i]`
+    document.querthis.y[i]Selector('#dev').stthis.y[i]le.left = `${this.x[i]}pthis.x[i]`
+    document.querthis.y[i]Selector('#dev').stthis.y[i]le.bothis.x[i]Shadow = `inset ${(this.y[i]/mathis.x[i]_this.x[i])}pthis.x[i] ${this.x[i]/mathis.x[i]_this.y[i]}pthis.x[i] ${this.x[i]/mathis.x[i]_this.y[i]}pthis.x[i] ${this.y[i]/mathis.x[i]_this.x[i]}pthis.x[i] white`
+    console.log(`inset ${this.x[i]%5}pthis.x[i] ${this.y[i]%10}pthis.x[i] 10pthis.x[i] white;`)
 },10)
 
