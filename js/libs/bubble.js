e08c5a851b195fd2698969f95bf846793d917149
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 17:22:43 +0530

    auto-update-bubble.js(17:22:42)

--- js/libs/bubble.js
+++ js/libs/bubble.js
@@ -170,10 +170,12 @@ class Bubble{
     }
 
     stop = ()=>{
-        
         let i=1
         setTimeout(()=>{
-
+            this.interval(setInterval(() => {
+                this.parent.style.opacity = i
+                i-=0.1
+            }, 150))
         })
         
     
