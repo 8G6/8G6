da88a0dcfe669a3c10ad3f6f956ff1760ae6a179
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 18:13:57 +0530

    auto-update-loader.js(18:13:55)

--- js/custom/loader.js
+++ js/custom/loader.js
@@ -12,7 +12,7 @@ function bubbleSetup(){
     bubble.randColorDelay = 25
     bubble.randSizeLim    = {start:50,end:150,step:25}
     bubble.size           = 50
-    bubble.y_offset       = 50
+    bubble.y_offset       = 5
     bubble.x_offset       = 50
     bubble.start()
 }
