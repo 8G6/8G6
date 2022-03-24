3c91acbf2bb6603ffe15148767848c0fc4cd2687
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 19:41:58 +0530

    auto-update-loader.js(19:41:56)

--- js/custom/loader.js
+++ js/custom/loader.js
@@ -17,7 +17,7 @@ function bubbleSetup(){
     let i=0
     setInterval(()=>{
         i+=1
-        $('.progress-bar-box').style.boxShadow   = `inset 50px 50px ${i}px #010f22`
+        $('.progress-bar-box').style.boxShadow   = `inset 50px 50px ${5}px #010f22`
         $('.progress-bar-box').backgroundIimage  = `linear-gradient(${i}deg, #029797, #0a0a0a)`
     },10)
 }
