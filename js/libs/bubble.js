f669143829df589ae69496f289cdea7a41b54a94
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 17:35:46 +0530

    auto-update-bubble.js(17:35:43)

--- js/libs/bubble.js
+++ js/libs/bubble.js
@@ -172,7 +172,7 @@ class Bubble{
     fadeOut = (speed = 10)=>{
         let i=1
         let delay = 150
-        let wait  = (i-(speed/100)) * delay
+        let wait  = (i-(speed/100)) * delay * 10
         console.log(delay)
         this.interval.push(setInterval(() => {
             this.parent.style.opacity = i
