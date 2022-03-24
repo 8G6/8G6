e908588e22e860e0b3cac4109b840ee78c9ab6e5
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 16:48:03 +0530

    auto-update-bubble.js(16:48:00)

--- js/libs/bubble.js
+++ js/libs/bubble.js
@@ -86,7 +86,6 @@ class Bubble{
     }
     init = ()=>{
         this.randSizes      = range(this.randSizeLim.start,this.randSizeLim.end,this.randSizeLim.step)
-        console.log(this.randSizeLim.start,this.randSizeLim.end,this.randSizeLim.step)
         this.parent.classList.toggle('bg')
         let i=0,div,y=0;
         for(i=0;i<this.bubble_count;i++){
