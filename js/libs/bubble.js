e2e2277e97d0370ab31163a17e03b8d14e7b76ef
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 20:25:40 +0530

    auto-update-bubble.js(20:25:37)

--- js/libs/bubble.js
+++ js/libs/bubble.js
@@ -151,7 +151,7 @@ class Bubble{
                                                        ${(this.colorSatIndex/100)*this.sizes[i]*this.x[i]/this.max_x}px 
                                                        ${(this.colorSatIndex/100)*this.sizes[i]*this.y[i]/this.max_y}px 
                                                        ${(this.colorSatIndex/100)*this.sizes[i]*this.x[i]/this.max_x}px 
-                                                       ${this.color[i]}`
+                                                       ${this.color[0]}`
         }
         this.c++
         if(this.c%this.randColorSpeed==0 && this.randColor){
