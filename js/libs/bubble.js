9d4d29a15dbf5995f09bb78354392bf9896747e0
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 16:38:40 +0530

    auto-update-bubble.js(16:38:39)

--- js/libs/bubble.js
+++ js/libs/bubble.js
@@ -6,11 +6,21 @@ LICENSE    : GNU General Public License v3.0
 ////////////////////////////////////////////////////////////*/
 
 
+let $     = (a,t=1)             => t ? document.querySelector(a) : document.querySelectorAll(a)
+let rand  = (a)                 => a[Math.floor(Math.random()*a.length)]
 let range = (start,end,step=1)  => {
     let arr = []
     let i;
     for(i=start;i<end+1;i+=step)
         arr.push(i)
+    return arr
+}
+let hex = toHex(0,256)
+function toHex(strat,end){
+    let arr=[];
+    for(i=strat;i<end+1;i++){
+        arr.push(i.toString(16))
+    }
     return arr
 }
 
