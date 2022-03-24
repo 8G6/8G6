406acd8defe7288122a3e3b9e1446fd2b6786656
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 17:10:56 +0530

    auto-update-bubble.js(17:10:55)

--- js/libs/bubble.js
+++ js/libs/bubble.js
@@ -26,7 +26,7 @@ function predict(time){
 function createStyle(Class,s){
     let style=document.createElement('style');
     style.innerHTML = Class
-    style = style
+    
     document.head.appendChild(style)
 }
 
