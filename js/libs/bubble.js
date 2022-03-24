079f34019eee69d4c26ba8cf6436e50e61924e13
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-03-24 17:15:32 +0530

    auto-update-bubble.js(17:15:31)

--- js/libs/bubble.js
+++ js/libs/bubble.js
@@ -177,6 +177,6 @@ class Bubble{
         document.body.classList.toggle('bg')
         this.parent.classList.toggle('bg')
         this.parent.innerHTML = ''
-        this.start
+        this.style.innerHTML  = ''
     }
 }
