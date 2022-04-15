ccebb453090f84038d3aebfb1de0d68d2a22dea5
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-04-15 21:25:49 +0530

    auto-update-mat_int.js(21:25:44)

--- js/custom/mat_int.js
+++ js/custom/mat_int.js
@@ -8,6 +8,6 @@ mat.fadeOut     = 1
 mat.fontColor   = 'green'
 mat.charArray   = ['*','#','-','*','#','-']
 mat.randomColor = false
-mat.timeout     = 400
+mat.timeout     = 120
 mat.rain        = true
 mat.itrate()
