e864d5d14951596adda8187fd0e31f611f605a26
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2022-04-15 22:54:55 +0530

    auto-update-loader.js(22:54:53)

---
+++ js/custom/bub_int.js
@@ -0,0 +1,12 @@
+
+let bubble              = new Bubble('main',50)
+bubble.color            = 10
+bubble.size             = 100
+bubble.x_speed          = 21
+bubble.y_speed          = 21
+bubble.colorSatIndex    = 20
+bubble.randSpeed        = 0
+bubble.randColor        = 1
+bubble.colorSync        = 1
+bubble.randColorDelay   = 200
+bubble.start()
